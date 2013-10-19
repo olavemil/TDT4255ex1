8598a77378bfa9ce53a7a80c10d05add06160908
x10an14 <x10an14@hotmail.com>
2013-10-19 12:24:17 +0200

    Addded hazard detection unit and forwarding unit to project (empty)

---
+++ processor/forwarding_unit.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:23:09 10/19/2013 
+-- Design Name: 
+-- Module Name:    forwarding_unit - behave 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity forwarding_unit is
+end forwarding_unit;
+
+architecture behave of forwarding_unit is
+
+begin
+
+
+end behave;
+
---
+++ processor/hazard_detection_unit.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:19:11 10/19/2013 
+-- Design Name: 
+-- Module Name:    hazard_detection_unit - behave 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hazard_detection_unit is
+end hazard_detection_unit;
+
+architecture behave of hazard_detection_unit is
+
+begin
+
+
+end behave;
+
