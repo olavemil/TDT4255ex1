1aafa5d19712a92cee1d983190403b30e46ccd6a
x10an14 <x10an14@hotmail.com>
2013-10-19 12:15:59 +0200

    Added pipeline stages to project, and re-organized folder-structure to reflect pipeline

---
+++ processor/pipeline_stage2/pipe_stage2.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:44:55 10/19/2013 
+-- Design Name: 
+-- Module Name:    pipe_stage2 - behave 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pipe_stage2 is
+end pipe_stage2;
+
+architecture behave of pipe_stage2 is
+
+begin
+
+
+end behave;
+
---
+++ processor/pipeline_stage4/pipe_stage4.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:45:39 10/19/2013 
+-- Design Name: 
+-- Module Name:    pipe_stage4 - behave 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pipe_stage4 is
+end pipe_stage4;
+
+architecture behave of pipe_stage4 is
+
+begin
+
+
+end behave;
+
