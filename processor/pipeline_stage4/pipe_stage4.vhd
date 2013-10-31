6fb3ec1c060b0eda9405b18f2ebcf7df5ce3ba06
unknown <olavemil@dmlab10.idipc.idi.ntnu.no>
2013-10-31 10:27:27 +0100

    Finalized stage 4 according to gdrive schematic

--- processor/pipeline_stage4/pipe_stage4.vhd
+++ processor/pipeline_stage4/pipe_stage4.vhd
@@ -34,40 +34,23 @@ entity pipe_stage4 is
 		wb_in				: in	STD_LOGIC;
 		wb_out				: out	STD_LOGIC;
 		
-		m_in				: in	STD_LOGIC;
 		alu_result_in		: in	STD_LOGIC_VECTOR(31 downto 0);
 		alu_result_out		: out	STD_LOGIC_VECTOR(31 downto 0);
 		
-		dmem_address_in		: in	STD_LOGIC_VECTOR(31 downto 0);
-		dmem_address_out	: out	STD_LOGIC_VECTOR(31 downto 0);
-		
-		reg_r_in			: in	STD_LOGIC_VECTOR(31 downto 0);--?
-		reg_r_out			: out	STD_LOGIC_VECTOR(31 downto 0);--?
-		
-		dmem_we_in			: in	STD_LOGIC;--?
-		dmem_we_out			: out	STD_LOGIC;--?
-		
-		dmem_re_in			: in	STD_LOGIC;--?
-		dmem_re_out			: out	STD_LOGIC;--?
-		
-		dmem_result_in		: in	STD_LOGIC_VECTOR(31 downto 0);
-		dmem_result_out		: out	STD_LOGIC_VECTOR(31 downto 0);
+		reg_r_in			: in	STD_LOGIC_VECTOR(31 downto 0);
+		reg_r_out			: out	STD_LOGIC_VECTOR(31 downto 0)
 	);
 end pipe_stage4;
 
 architecture behave of pipe_stage4 is
 
 begin
-	--alu_result_out		<= alu_result_in;
-	--dmem_address_out	<= dmem_address_in;
-	
 	registers : process(clk)
 		begin
 			if rising_edge(clk) then
 				wb_out			<= wb_in;
 				reg_r_out		<= reg_r_in;
 				alu_result_out	<= alu_result_in;
-				dmem_result_out	<= dmem_result_in;
 			end if;
 		end process;
 end behave;
